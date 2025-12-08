0ac3fd30e62418b0f9adc2a524aab0df0b28b60a
Nelson <huynhyohan@gmail.com>
2025-12-08 13:56:32 +0700

    Delete App directory

--- App/DATA/debug_columns.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-DATA_FILE = r"C:\Users\Nelson\OneDrive\Desktop\2. Areas\PricingSystem\App\DATA\Data.xlsx"
-
-df = pd.read_excel(DATA_FILE, header=[0, 1])
-
-print("\n=== Danh sách TÊN CỘT thực tế ===\n")
-for col in df.columns:
-    print(col)
